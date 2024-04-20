2cce5294e25855a7587b6cda38758129ba5c66e6
Kusanagies <Sylvainhuang3@gmail.com>
2024-04-21 01:26:02 +0200

    Modification de la barre de menu

--- barre-menu.php
+++ barre-menu.php
@@ -1,5 +1,7 @@
 <div class="menu">
     <a href="accueil.php"><img src="IMG/LogoAccueil.png" alt="Logo"></a>
     <a href="reservation.php">Reservation</a>
+    <a href="aboutus.php">Et nous?</a>
+    <a href="joinus.php">Nous rejoindre!</a>
     <a href="compte.php">Mon compte</a>
 </div>
