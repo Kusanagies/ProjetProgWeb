1d4b7b1ddfd1942c6c5af952889fea18a84c3d11
Kusanagies <Sylvainhuang3@gmail.com>
2024-05-03 15:37:37 +0200

    rien a dire

--- accueil.php
+++ accueil.php
@@ -8,30 +8,7 @@
 </head>
  <style>
 
-
-      
-        table {
-            width: 100%;
-            height: 100%;
-            border-collapse: collapse; 
-        }
-
-       
-        td {
-            border: 1px solid black; 
-            text-align: center;     
-            vertical-align: middle; 
-        }
-
-       
-        tr:first-child {
-            height: 10%; 
-        }
-        
-        tr:last-child {
-            height: 90%; 
-        }
-    </style>
+</style>
 <body>
 
 <table>
